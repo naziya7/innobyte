5a571e995daf54587d47928fafe9167ca54a60fc
naziya <naziya.patel@gaiansolutions.com>
2024-07-22 11:29:13 +0530

    backend and frontend folders added and server side config

---
+++ server/index.js
@@ -0,0 +1,37 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const app = express();
+const cookieParser = require("cookie-parser");
+const authRoute = require("./Routes/AuthRoute");
+const MONGO_URL = process.env.MONGO_URL;
+const PORT = process.env.PORT || 3000;
+
+mongoose.connect(MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => console.log("MongoDB is connected successfully"))
+.catch((err) => console.error(err));
+
+app.listen(PORT, (err) => {
+  if (err) {
+    console.error("Failed to start server:", err);
+    return;
+  }
+  console.log(`Server is listening on port ${PORT}`);
+});
+
+app.use(
+  cors({
+    origin: ["http://localhost:8080"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
+app.use(express.json());
+app.use(cookieParser());
+app.use("/", authRoute);
+
